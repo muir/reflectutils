3430982b45975162a90dc213f07ce44d569838bc
David Sharnoff <muir@idiom.org>
2021-10-31 23:05:20 -0700

    add MakeStringSetter; add github actions

---
+++ unpack.go
@@ -0,0 +1,148 @@
+package reflectutils
+
+import (
+	"encoding"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+var textUnmarshallerType = reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem()
+
+// MakeStringSetter handles setting a reflect.Value from a string.
+// Based on type, it returns a function to do the work.  It is assumed that the
+// reflect.Type matches the reflect.Value.  If not, panic is likely.
+//
+// For arrays and slices, strings are split on comma to create the values for the
+// elements.
+//
+// Anything that implements encoding.TextUnmarshaler will be unpacked that way.
+//
+// Maps, structs, channels, interfaces, channels, and funcs are not supported unless
+// they happen to implent encoding.TextUnmarshaler.
+func MakeStringSetter(t reflect.Type) (func(target reflect.Value, value string) error, error) {
+	if t.AssignableTo(textUnmarshallerType) {
+		return func(target reflect.Value, value string) error {
+			p := reflect.New(t.Elem())
+			target.Set(p)
+			err := target.Interface().(encoding.TextUnmarshaler).UnmarshalText([]byte(value))
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			return nil
+		}, nil
+	}
+	if reflect.PtrTo(t).AssignableTo(textUnmarshallerType) {
+		return func(target reflect.Value, value string) error {
+			err := target.Addr().Interface().(encoding.TextUnmarshaler).UnmarshalText([]byte(value))
+			return errors.WithStack(err)
+		}, nil
+	}
+	switch t.Kind() {
+	case reflect.Ptr:
+		setElem, err := MakeStringSetter(t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		return func(target reflect.Value, value string) error {
+			p := reflect.New(t.Elem())
+			target.Set(p)
+			err := setElem(target.Elem(), value)
+			if err != nil {
+				return err
+			}
+			return nil
+		}, nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return func(target reflect.Value, value string) error {
+			i, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			target.SetInt(i)
+			return nil
+		}, nil
+	case reflect.Uint, reflect.Uintptr, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return func(target reflect.Value, value string) error {
+			i, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			target.SetUint(i)
+			return nil
+		}, nil
+	case reflect.Float32, reflect.Float64:
+		return func(target reflect.Value, value string) error {
+			f, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			target.SetFloat(f)
+			return nil
+		}, nil
+	case reflect.String:
+		return func(target reflect.Value, value string) error {
+			target.SetString(value)
+			return nil
+		}, nil
+	case reflect.Complex64, reflect.Complex128:
+		return func(target reflect.Value, value string) error {
+			c, err := strconv.ParseComplex(value, 128)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			target.SetComplex(c)
+			return nil
+		}, nil
+	case reflect.Bool:
+		return func(target reflect.Value, value string) error {
+			b, err := strconv.ParseBool(value)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			target.SetBool(b)
+			return nil
+		}, nil
+	case reflect.Array:
+		setElem, err := MakeStringSetter(t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		return func(target reflect.Value, value string) error {
+			for i, v := range strings.SplitN(value, ",", target.Cap()) {
+				err := setElem(target.Index(i), v)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}, nil
+	case reflect.Slice:
+		setElem, err := MakeStringSetter(t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		return func(target reflect.Value, value string) error {
+			values := strings.Split(value, ",")
+			a := reflect.MakeSlice(target.Type(), len(values), len(values))
+			for i, v := range values {
+				err := setElem(a.Index(i), v)
+				if err != nil {
+					return err
+				}
+			}
+			target.Set(a)
+			return nil
+		}, nil
+	case reflect.Map:
+		fallthrough
+	case reflect.Struct:
+		fallthrough
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Invalid, reflect.UnsafePointer:
+		fallthrough
+	default:
+		return nil, errors.Errorf("type %s not supported", t)
+	}
+}
---
+++ unpack_test.go
@@ -0,0 +1,89 @@
+package reflectutils_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/muir/reflectutils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Foo string
+
+func (fp *Foo) UnmarshalText(b []byte) error {
+	*fp = Foo("~" + string(b) + "~")
+	return nil
+}
+
+func TestStringSetter(t *testing.T) {
+	type tsType struct {
+		Int        int         `value:"38"`
+		Int8       int8        `value:"-9"`
+		Int16      int16       `value:"329"`
+		Int32      int32       `value:"-32902"`
+		Int64      int64       `value:"3292929"`
+		Uint       uint        `value:"202"`
+		Uint8      uint8       `value:"99"`
+		Uint16     uint16      `value:"3020"`
+		Uint32     uint32      `value:"92020"`
+		Uint64     uint64      `value:"320202"`
+		Float32    float32     `value:"3.9"`
+		Float64    float64     `value:"4.32e7" want:"4.32e+07"`
+		String     string      `value:"foo"`
+		Bool       bool        `value:"false"`
+		IntP       *int        `value:"-82"`
+		Int8P      *int8       `value:"-2"`
+		Int16P     *int16      `value:"-39"`
+		Int32P     *int32      `value:"-329"`
+		Int64P     *int64      `value:"-3292"`
+		UintP      *uint       `value:"239"`
+		Uint8P     *uint8      `value:"92"`
+		Uint16P    *uint16     `value:"330"`
+		Uint32P    *uint32     `value:"239239"`
+		Uint64P    *uint64     `value:"3923"`
+		Float32P   *float32    `value:"3.299"`
+		Float64P   *float64    `value:"9.2"`
+		StringP    *string     `value:"foop"`
+		Complex64  *complex64  `value:"4+3i"     want:"(4+3i)"`
+		Complex128 *complex128 `value:"3.9+2.6i" want:"(3.9+2.6i)"`
+		BoolP      *bool       `value:"true"`
+		IntSlice   []int       `value:"3,9,-10"  want:"[3 9 -10]"`
+		IntArray   [2]int      `value:"22,11"    want:"[22 11]"`
+		Foo        Foo         `value:"foo"      want:"~foo~"`
+		FooArray   [2]Foo      `value:"a,b,c"    want:"[~a~ ~b,c~]"`
+		FooP       *Foo        `value:"foo"      want:"~foo~"`
+	}
+	var ts tsType
+	vp := reflect.ValueOf(&ts)
+	v := reflect.Indirect(vp)
+	var count int
+	reflectutils.WalkStructElements(v.Type(), func(f reflect.StructField) bool {
+		t.Logf("field %s, a %s", f.Name, f.Type)
+		value, ok := f.Tag.Lookup("value")
+		if !assert.Truef(t, ok, "input value for %s", f.Name) {
+			return true
+		}
+		want, ok := f.Tag.Lookup("want")
+		if !ok {
+			want = value
+		}
+		fn, err := reflectutils.MakeStringSetter(f.Type)
+		if !assert.NoErrorf(t, err, "make string setter for %s", f.Name) {
+			return true
+		}
+		e := v.FieldByIndex(f.Index)
+		err = fn(e, value)
+		if assert.NoError(t, err, "set %s to '%s'", f.Name, value) {
+			ge := e
+			if f.Type.Kind() == reflect.Ptr {
+				ge = e.Elem()
+			}
+			assert.Equalf(t, want, fmt.Sprint(ge.Interface()), "got setting %s to '%s'", f.Name, value)
+		}
+		count++
+		return true
+	})
+	assert.Equal(t, v.NumField(), count, "number of fields tested")
+}
